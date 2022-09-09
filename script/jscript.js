7721af4ba84e9936ccc3720ff7b89d6ae19e302d
Giacii <93996172+Giaciii@users.noreply.github.com>
2022-09-09 16:38:28 +0200

    Miner 2

--- script/jscript.js
+++ script/jscript.js
@@ -4,7 +4,11 @@ function m_geld() {
     document.getElementById("a_geld").innerHTML = geld += 1;
 
     if (geld >= 10) {
-        document.getElementById("miners").style.visibility = "visible";
+        document.getElementById("miner1").style.visibility = "visible";
+    }
+
+    if (geld >= 100) {
+        document.getElementById("miner2").style.visibility = "visible";
     }
 };
 
@@ -13,10 +17,26 @@ m1_level = 1;
 function miner1() {
     if (m1_level * 10 <= geld) {
         geld -= m1_level * 10;
-        document.getElementById("level").innerHTML = m1_level += 1;
+        document.getElementById("m1_level").innerHTML = m1_level += 1;
         
         setInterval(() => {
             document.getElementById("a_geld").innerHTML = geld += 1;
+        }, 1000)
+    
+    } else {
+        alert("Zu wenig Geld");
+    }
+}
+
+m2_level = 1;
+
+function miner2() {
+    if (m2_level * 100 <= geld) {
+        geld -= m2_level * 100;
+        document.getElementById("m2_level").innerHTML = m2_level += 1;
+        
+        setInterval(() => {
+            document.getElementById("a_geld").innerHTML = geld += 5;
         }, 1000)
     
     } else {
